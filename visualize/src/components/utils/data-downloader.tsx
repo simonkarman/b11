3c57a6be27454ee0514aa2d0991674ca198d3941
Simon Karman <mail@simonkarman.nl>
2024-11-23 21:55:00 +0100

    Switch colors of Yorick and Raoul

--- visualize/src/components/utils/data-downloader.tsx
+++ visualize/src/components/utils/data-downloader.tsx
@@ -3,9 +3,9 @@ import { PropsWithChildren } from 'react';
 
 export const everyone = ['raoul', 'thomas', 'yorick', 'robin', 'simon', 'rogier'] as const;
 export const colors = {
-  raoul: { rgb: 'rgb(67 56 202)', border: 'border-indigo-700', textClass: 'text-indigo-700', bgClass: 'bg-indigo-700' },
+  raoul: { rgb: 'rgb(14 116 144)', border: 'border-cyan-700', textClass: 'text-cyan-700', bgClass: 'bg-cyan-700' },
   thomas: { rgb: 'rgb(21 128 61)', border: 'border-green-700', textClass: 'text-green-700', bgClass: 'bg-green-700' },
-  yorick: { rgb: 'rgb(14 116 144)', border: 'border-cyan-700', textClass: 'text-cyan-700', bgClass: 'bg-cyan-700' },
+  yorick: { rgb: 'rgb(67 56 202)', border: 'border-indigo-700', textClass: 'text-indigo-700', bgClass: 'bg-indigo-700' },
   robin: { rgb: 'rgb(126 34 206)', border: 'border-purple-700', textClass: 'text-purple-700', bgClass: 'bg-purple-700' },
   simon: { rgb: 'rgb(180 83 9)', border: 'border-amber-700', textClass: 'text-amber-700', bgClass: 'bg-amber-700' },
   rogier: { rgb: 'rgb(185 28 28)', border: 'border-red-700', textClass: 'text-red-700', bgClass: 'bg-red-700' },
